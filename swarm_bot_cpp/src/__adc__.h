f191c82a4fb410d79331dd102a7cf5f25e01c616
Adnan Saood <adnan.saood@outlook.com>
2021-05-28 12:37:26 +0300

    ADC reform

--- swarm_bot_cpp/src/__adc__.h
+++ swarm_bot_cpp/src/__adc__.h
@@ -1,30 +1,12 @@
-/*
- * __adc__.h
- *
- * Created: 5/9/2021 9:44:28 AM
- *  Author: adnan
- */ 
-
-
 #ifndef __ADC___H_
 #define __ADC___H_
 
 //#define __ADC_RES_8_BIT
 #define __ADC_RES_10_BIT
 
-
-#include <stdio.h>
-#include <asf.h>
-#include <avr/io.h>
-#include <avr/interrupt.h>
-#include <board.h>
-#include <conf_board.h>
-#include <util/delay.h>
-
-
-
+#include <__swarm_wold__.h>
 
 void _adc_init(void);
-int _adc_read(char channel);
+int _adc_read(uint8_t channel);
 
 #endif /* __ADC___H_ */
